e9433e56c9dbd8a6ff6aef966770cb7adf95a9ee
Puschkarski Severin <severin@puschkarski.org>
2015-09-21 16:02:48 +0200

    Fix useref paths

--- gulp/tasks/useref.js
+++ gulp/tasks/useref.js
@@ -1,5 +1,6 @@
 'use strict';
 var gulp  = require('gulp');
+var debug  = require('gulp-debug');
 var config= require('../config');
 
 var useref = require('gulp-useref'); //usemin was not reliable
@@ -15,13 +16,16 @@ gulp.task('useref', function() {
   var assets = useref.assets();
   
   return gulp.src('index.html')
+    .pipe(debug({title: 'useref_in:'}))
     .pipe(assets)
+    .pipe(debug({title: 'useref_assets:'}))
     .pipe(gulpif('*.js', uglify()))
     .pipe(gulpif('*.css', minifyCss()))
     .pipe(assets.restore())
     .pipe(useref())
     //.pipe(gulpif('*.html',replace({patterns:[{match: /<html/, replacement: '<html manifest=manifest.appcache'}]})))
     //.pipe(gulpif('*.html',minifyHtml({empty: true})))
+    //.pipe(debug({title: 'useref_min:'}))
     .pipe(gulp.dest('www'));
 });
 
